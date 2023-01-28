51af3456e7aba024195400600598207fcca6ba75
JLarky <jlarky@gmail.com>
2023-01-28 16:19:55 -0700

    qgp

---
+++ template-astro-vite-cra-ts/README.md
@@ -0,0 +1,3 @@
+# QGP — Astro superpowered by Vite HMR
+
+https://qgp.deno.dev/
--- template-astro-vite-cra-ts/astro.config.mjs
+++ template-astro-vite-cra-ts/astro.config.mjs
@@ -1,4 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import { defineAstro } from 'qgp';
+
+import { common } from './qgp.config.mjs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react()],
+	vite: defineAstro(common, {}),
+	server: { port: 3000 },
+});
