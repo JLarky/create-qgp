60bdb857f629005c591411d40312b72dd8d5baa7
JLarky <jlarky@gmail.com>
2023-02-10 17:26:58 -0700

    solidify

--- template-astro-vite-solid-ssr/astro.config.mjs
+++ template-astro-vite-solid-ssr/astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
 import { defineAstro } from 'qgp';
 
 import { common } from './qgp.config.mjs';
@@ -8,7 +8,7 @@ import { common } from './qgp.config.mjs';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [react()],
+	integrations: [solid()],
 	vite: defineAstro(common, {}),
 	server: { port: 3000 },
 });
---
+++ template-astro-vite-solid-ssr/src/qgp/solid.tsx
@@ -0,0 +1,6 @@
+import '../index.css';
+import SolidSPA from '../App';
+
+export const App = () => {
+	return <SolidSPA />;
+};
