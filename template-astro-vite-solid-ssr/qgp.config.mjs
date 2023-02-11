c791a68d4c37237b55b64af1085f7539b164348c
JLarky <jlarky@gmail.com>
2023-02-10 17:03:28 -0700

    create based on clean template

---
+++ template-astro-vite-solid-ssr/qgp.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import react from '@vitejs/plugin-react';
+import { defineCommon, defineVite } from 'qgp';
+import checker from 'vite-plugin-checker';
+
+export const common = defineCommon({
+	vite: {},
+});
+
+export default defineVite(common, {
+	plugins: [
+		react(),
+		checker({
+			typescript: true,
+			overlay: { initialIsOpen: false },
+		}),
+	],
+	server: { port: 5173 },
+});
